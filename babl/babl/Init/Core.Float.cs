9cdddacff847790816d41b85db0beede011a286d
Stefan Kewatt <stefan.kewatt@gmail.com>
2020-10-21 18:20:28 -0500

    Added float conversions.

--- babl/babl/Init/Core.Float.cs
+++ babl/babl/Init/Core.Float.cs
@@ -10,9 +10,62 @@ namespace babl.Init
 {
     internal static partial class Core
     {
+        private static void ConvertDoubleFloat(Babl _1, object src, object dst, int srcPitch, int dstPitch, long num, object _2)
+        {
+            if (src is ReadOnlyMemory<double> srcMem &&
+                dst is Memory<float> dstMem)
+            {
+                var srcSpan = srcMem.Span;
+                var dstSpan = dstMem.Span;
+
+                while (num-- is > 0)
+                {
+                    dstSpan[0] = (float)srcSpan[0];
+                    dstSpan = dstSpan[dstPitch..];
+                    srcSpan = srcSpan[srcPitch..];
+                }
+            }
+        }
+        private static void ConvertFloatDouble(Babl _1, object src, object dst, int srcPitch, int dstPitch, long num, object _2)
+        {
+            if (src is ReadOnlyMemory<float> srcMem &&
+                dst is Memory<double> dstMem)
+            {
+                var srcSpan = srcMem.Span;
+                var dstSpan = dstMem.Span;
+
+                while (num-- is > 0)
+                {
+                    dstSpan[0] = srcSpan[0];
+                    dstSpan = dstSpan[dstPitch..];
+                    srcSpan = srcSpan[srcPitch..];
+                }
+            }
+        }
+        private static void ConvertFloatFloat(Babl _1, object src, object dst, int srcPitch, int dstPitch, long num, object _2)
+        {
+            if (src is ReadOnlyMemory<float> srcMem &&
+                dst is Memory<float> dstMem)
+            {
+                var srcSpan = srcMem.Span;
+                var dstSpan = dstMem.Span;
+
+                while (num-- is > 0)
+                {
+                    dstSpan[0] = srcSpan[0];
+                    dstSpan = dstSpan[dstPitch..];
+                    srcSpan = srcSpan[srcPitch..];
+                }
+            }
+        }
         private static void TypeFloatInit()
         {
             CreateType("float", id: Float, bits: 32, doc: "IEEE 754 single precision");
+
+            CreateConversion(Conversion(Float), Conversion(Ids.Double), plane: ConvertFloatDouble);
+            CreateConversion(Conversion(Ids.Double), Conversion(Float), plane: ConvertDoubleFloat);
+            CreateConversion(Conversion(Float), Conversion(Float), plane: ConvertFloatFloat);
+
         }
     }
 }
