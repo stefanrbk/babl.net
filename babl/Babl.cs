9c902fcae88d56fd8bbd969afbd1ea6eb2f5f1bf
stefanrbk <stefan.kewatt@gmail.com>
2021-06-19 20:30:42 -0500

    Added Babl base class and ClassType enum

---
+++ babl/Babl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babl
+{
+    public abstract class Babl
+    {
+        internal const int Magic = 0xbab100;
+
+        internal ClassType Id;
+        internal string Name;
+    }
+
+    enum ClassType : int
+    {
+        Instance = Babl.Magic,
+        Type,
+        TypeInteger,
+        TypeFloat,
+        Sampling,
+        Trc,
+        Component,
+        Model,
+        Format,
+        Space,
+
+        Conversion,
+        ConversionLinear,
+        ConversionPlane,
+        ConversionPlanar,
+
+        Fish,
+        FishReference,
+        FishSimple,
+        FishPath,
+        Image,
+
+        Extension,
+
+        Sky
+    }
+}
