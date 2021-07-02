234cad10890eec463068f0d38383e7773473b356
stefanrbk <stefan.kewatt@gmail.com>
2021-07-02 08:47:04 -0500

    Added Util functions

---
+++ babl/Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babl
+{
+    static class Util
+    {
+        public static double LinearToGamma2dot2(double value) =>
+            Math.Pow(value, 1 / 2.2);
+
+        public static double Gamma2dot2ToLinear(double value) =>
+            Math.Pow(value, 2.2);
+        public static float LinearToGamma2dot2(float value) =>
+            MathF.Pow(value, 1 / 2.2f);
+
+        public static float Gamma2dot2ToLinear(float value) =>
+            MathF.Pow(value, 2.2f);
+    }
+}
